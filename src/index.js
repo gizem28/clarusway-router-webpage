39050c58e92692e4061df402093e633b80ba3189
gizem28 <gizemodun28@gmail.com>
2021-12-28 23:49:21 +0100

    m

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+//css style icin parent app/index birine import edilmeli
+// import "bootstrap/dist/css/bootstrap.min.css"; 
 
 ReactDOM.render(
   <React.StrictMode>
