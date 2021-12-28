9b41d7a1238cbfb5a08d230c4acdab92e8a7e439
gizem28 <gizemodun28@gmail.com>
2021-12-29 00:50:57 +0100

    m

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import MyNav from "./components/MyNav";
 import Home from "./pages/Home";
+import Slider from "./components/Slider";
 
 function App() {
   return (
     <div>
       <MyNav />
+      <Slider/>
       <Home />
     </div>
   );
