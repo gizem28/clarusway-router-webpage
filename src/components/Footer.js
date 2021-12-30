a9abcce2b8156b79fa1270ba489ef1e0a8b54fbb
gizem28 <gizemodun28@gmail.com>
2021-12-30 01:09:45 +0100

    m

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer className='text-center bg-dark p-3'
+        style={{marginTop:"30px", position:"fixed", bottom:"0", width:"100%"}}>
+            <p className='text-light'>
+                Copyright 2021
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Courses from '../components/Courses'
+import Courses from '../components/Courses'; //?
+import Slider from "../components/Slider";
 
 const Home = () => {
     return (
         <div>
             <Courses/>
+            <Slider/>
         </div>
     )
 }
